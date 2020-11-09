5ccc786ff4d0d61fc19b31753527b80a842a62f0
alinayadova <70139264+alinayadova@users.noreply.github.com>
2020-11-09 14:44:25 +0200

    changed eventCreationTest

--- src/test/java/com/sch/tests/EventCreationTests.java
+++ src/test/java/com/sch/tests/EventCreationTests.java
@@ -7,37 +7,56 @@ import org.testng.annotations.Test;
 public class EventCreationTests extends TestBase {
     @BeforeMethod
     public void preconditions() throws InterruptedException {
-        if(app.getUserHelper().isLoginButtonPresent()){
+        if (app.getUserHelper().isLoginButtonPresent()) {
             app.getUserHelper().defaultLogin();
         }
     }
 
-    @Test
-    public void testEventCreation(){
-        app.getEventHelper().clickOnPlusButton();
-        app.getEventHelper().clickOnPencil();
-        app.getEventHelper().fillEventCreationForm("ala", "1", 3, "500");
-        app.getEventHelper().clickOnAddEventButton();
-    }
+        @Test
+        public void testEventCreation(){
+            /*
+             * tap on Plus button
+             * tapOnPencil
+             * fillEventCreationForm
+             * TapOnAddEventButton
+             * */
+            app.getEventHelper().clickOnPlusButton();
+            app.getEventHelper().clickOnPencil();
 
-    @Test
-    public void testEventCreationChangeDate(){
-        //int before = app.getEventHelper().getEventCountByMonth("November");
-        app.getEventHelper().clickOnPlusButton();
-        app.getEventHelper().clickOnPencil();
-        app.getEventHelper().swipeCalendarToLeft();
-        app.getEventHelper().selectDate("future","NOVEMBER", "10");
+            app.getEventHelper().fillEventCreationForm("ahj", "1", 3, "150");
+            app.getEventHelper().clickOnAddEventButton();
+            
+        }
 
-        app.getEventHelper().fillEventCreationForm("aаjjh", "1", 3, "50");
-        app.getEventHelper().clickOnAddEventButton();
+        @Test
+        public void testEventCreationChangeDate(){
 
-        Assert.assertTrue(app.getEventHelper().isEventPresent());
-        int after = app.getEventHelper().getEventCountByMonth("November");
+            //  int before = app.getEventHelper().getEventCountByMonth("November");
+            app.getEventHelper().clickOnPlusButton();
+            app.getEventHelper().clickOnPencil();
+            app.getEventHelper().swipeCalendarToLeft();
 
-        //   Assert.assertEquals(after, before+1 );
+       /*string month, String day (Dec 31)
+        list of dates (3)
+        (for int i = 0; i<list.size; i++){
+        String month = ...
+        while(month!=month){
+        tapOnArrow();
+        }
+        while day!= day
+        }
+     */
+            app.getEventHelper().selectDate("future","NOVEMBER", "10");
 
+            app.getEventHelper().fillEventCreationForm("aaadaaa", "1", 0, "50");
+            app.getEventHelper().clickOnAddEventButton();
 
-    }
+            Assert.assertTrue(app.getEventHelper().isEventPresent());
+            int after = app.getEventHelper().getEventCountByMonth("November");
+
+            //   Assert.assertEquals(after, before+1 );
+
+        }
 
 
 }
